8d6339d2b5b5c2dc17de7fdc60aa8b437462fc1c
Sean Dague <sean@dague.net>
2012-01-13 21:42:21 -0500

    fix to support arbitray formats on the date side

--- js/smart_dates.js
+++ js/smart_dates.js
@@ -1,6 +1,6 @@
 var SmartDates = SmartDates || {};
 
-SmartDates.register_dates = function(date1, date2) {
+SmartDates.register_dates = function(date1, date2, format) {
     var date1start = Date.parse(date1.val());
 
     date1.change(function() {
@@ -9,12 +9,12 @@ SmartDates.register_dates = function(date1, date2) {
 
         var interval = date2now.valueOf() - date1start.valueOf();
         // alert("Date1start: " + date1start + "\nDate2now: " + date2now +  "\nDate1now: " + date1now + "\nInterval: " + interval);
-        date2.val(date1now.clone().addMilliseconds(interval).toString("MM/dd/yyyy"));
+        date2.val(date1now.clone().addMilliseconds(interval).toString(format));
         date1start = Date.parse(date1.val());
     });
 };
 
-SmartDates.register_times = function(time1, time2) {
+SmartDates.register_times = function(time1, time2, format) {
     var time1start = Date.parse(time1.val());
 
     time1.change(function() {
@@ -23,13 +23,13 @@ SmartDates.register_times = function(time1, time2) {
 
         // if we started at the same point
         if (time2now.valueOf() == time1start.valueOf()) {
-            time2.val(time1now.clone().addHours(2).toString("hh:mmtt"));
+            time2.val(time1now.clone().addHours(2).toString(format));
             time1start = Date.parse(time1.val());
         }
         else {
             var interval = time2now.valueOf() - time1start.valueOf();
             // alert("Time1start: " + time1start + "\nTime2now: " + time2now +  "\nTime1now: " + time1now + "\nInterval: " + interval);
-            time2.val(time1now.clone().addMilliseconds(interval).toString("hh:mmtt"));
+            time2.val(time1now.clone().addMilliseconds(interval).toString(format));
             time1start = Date.parse(time1.val());
         }
     });
