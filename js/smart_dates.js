1845e3e7c339b0726a6d8d45afad2c355b567221
Sean Dague <sean@dague.net>
2012-01-14 14:29:40 -0500

    fix date parsing which got goofy on 12:00PM, moving to parseExact thanks to Chris Joslyn for helping me figure this out

--- js/smart_dates.js
+++ js/smart_dates.js
@@ -1,36 +1,39 @@
 var SmartDates = SmartDates || {};
 
 SmartDates.register_dates = function(date1, date2, format) {
-    var date1start = Date.parse(date1.val());
+    var date1start = Date.parseExact(date1.val(), format);
 
     date1.change(function() {
-        var date2now = Date.parse(date2.val());
-        var date1now = Date.parse(date1.val());
+        var date2now = Date.parseExact(date2.val(), format);
+        var date1now = Date.parseExact(date1.val(), format);
 
         var interval = date2now.valueOf() - date1start.valueOf();
         // alert("Date1start: " + date1start + "\nDate2now: " + date2now +  "\nDate1now: " + date1now + "\nInterval: " + interval);
         date2.val(date1now.clone().addMilliseconds(interval).toString(format));
-        date1start = Date.parse(date1.val());
+        date1start = Date.parseExact(date1.val(), format);
     });
 };
 
 SmartDates.register_times = function(time1, time2, format) {
-    var time1start = Date.parse(time1.val());
+    var time1start = Date.parseExact(time1.val(), format);
 
     time1.change(function() {
-        var time2now = Date.parse(time2.val());
-        var time1now = Date.parse(time1.val());
+        var time1now = Date.parseExact(time1.val(), format);
+        var time2now = Date.parseExact(time2.val(), format);
 
-        // if we started at the same point
-        if (time2now.valueOf() == time1start.valueOf()) {
+        if (time2now) {
+            // if we started at the same point
+            if (time2now.valueOf() == time1start.valueOf()) {
+                time2.val(time1now.clone().addHours(2).toString(format));
+            }
+            else {
+                var interval = time2now.valueOf() - time1start.valueOf();
+                time2.val(time1now.clone().addMilliseconds(interval).toString(format));
+            }
+        } else {
             time2.val(time1now.clone().addHours(2).toString(format));
-            time1start = Date.parse(time1.val());
         }
-        else {
-            var interval = time2now.valueOf() - time1start.valueOf();
-            // alert("Time1start: " + time1start + "\nTime2now: " + time2now +  "\nTime1now: " + time1now + "\nInterval: " + interval);
-            time2.val(time1now.clone().addMilliseconds(interval).toString(format));
-            time1start = Date.parse(time1.val());
-        }
+        time1start = Date.parseExact(time1.val(), format);
+
     });
 }
